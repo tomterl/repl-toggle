788d4ca3564bc79d9ae75deddbc07453fd67cbac
Tom Regner <tom@goochesa.de>
2016-01-19 11:04:48 +0100

    Get versions/tags straight.

--- repl-toggle.el
+++ repl-toggle.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Regner <tom@goochesa.de>
 ;; Maintainer: Tom Regner <tom@goochesa.de>
-;; Version: 0.3.0
+;; Version: 0.3.2
 ;; Keywords: repl, buffers, toggle
 ;; Package-Requires: ((fullframe  "0.0.5"))
 
