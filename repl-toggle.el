0249c2a72e6bf782c2c15b0cb1d925410543184f
Tom Regner <tom@goochesa.de>
2016-01-19 13:21:53 +0100

    Version bump (0.3.3)

--- repl-toggle.el
+++ repl-toggle.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Regner <tom@goochesa.de>
 ;; Maintainer: Tom Regner <tom@goochesa.de>
-;; Version: 0.3.2
+;; Version: 0.3.3
 ;; Keywords: repl, buffers, toggle
 ;; Package-Requires: ((fullframe  "0.0.5"))
 
