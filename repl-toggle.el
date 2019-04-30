a36caac7649fbffbe30f7b06541c9efd723563fc
Tom Regner <tom@goochesa.de>
2019-04-30 09:13:34 +0200

    Version Bump (0.6.1)
    Update the version header in the .el-file

--- repl-toggle.el
+++ repl-toggle.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Regner <tom@goochesa.de>
 ;; Maintainer: Tom Regner <tom@goochesa.de>
-;; Version: 0.5.0
+;; Version: 0.6.1
 ;; Keywords: repl, buffers, toggle
 ;; Package-Requires: ((fullframe  "0.0.5"))
 
